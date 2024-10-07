5e32e3e5c83562c4b626fb3fb91daad1eec64fcc
Nicanor Fernandez <nicanor.fernandez@est.fi.uncoma.edu.ar>
2024-10-07 00:23:03 -0300

    Página de catálogo inicial con js

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+//Productos a ofrecer
+
+const productos = [
+    { id: 1, nombre: 'Remera Manga Corta', descripcion: 'Remera Manga Corta de Algodon', precio: 20, imagen: '../img/Remera-manga-corta.jpg' },
+    { id: 2, nombre: 'Bermuda', descripcion: 'Bermuda de Jean', precio: 80, imagen: '../img/bermuda-de-jean.png' },
+    { id: 2, nombre: 'Bermuda', descripcion: 'Bermuda de Jean', precio: 80, imagen: '../img/bermuda-de-jean.png' },
+    //{ id: 2, nombre: 'Bermuda', descripcion: 'Bermuda de Jean', precio: 80, imagen: '../img/bermuda-de-jean.png' },
+    //{ id: 2, nombre: 'Bermuda', descripcion: 'Bermuda de Jean', precio: 80, imagen: '../img/bermuda-de-jean.png' },
+    //{ id: 2, nombre: 'Bermuda', descripcion: 'Bermuda de Jean', precio: 80, imagen: '../img/bermuda-de-jean.png' },
+    //{ id: 2, nombre: 'Bermuda', descripcion: 'Bermuda de Jean', precio: 80, imagen: '../img/bermuda-de-jean.png' }
+];
+
+//Genero el catalogo
+function cargarCatalogo() {
+    const catalogo = document.getElementById('catalogo');
+
+    productos.forEach(producto => {
+        //Creo la estructura
+        const divProducto = document.createElement('div');
+        divProducto.classList.add('producto');
+
+        divProducto.innerHTML = `
+            <img src="${producto.imagen}" alt="${producto.nombre}">
+            <h3>${producto.nombre}</h3>
+            <p>${producto.descripcion}</p>
+            <p class="precio">$${producto.precio}</p>
+            <button>Comprar</button>
+        `
+        catalogo.appendChild(divProducto);
+    })
+}
+
+window.onload = cargarCatalogo;
