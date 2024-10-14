d754a6f409e74551bb43b16ca39bffeef5510300
Sebastian Reibold <sebastian.reibold@est.fi.uncoma.edu.ar>
2024-10-14 20:36:30 -0300

    Rediseño de pagina Catalogos

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 //Productos a ofrecer
 
 const productos = [
-    { id: 1, nombre: 'Remera Manga Corta', descripcion: 'Remera Manga Corta de Algodon', precio: 20, imagen: '../img/Remera-manga-corta.jpg' },
-    { id: 2, nombre: 'Bermuda', descripcion: 'Bermuda de Jean', precio: 80, imagen: '../img/bermuda-de-jean.png' },
-    { id: 3, nombre: 'Bermuda', descripcion: 'Bermuda de Jean', precio: 80, imagen: '../img/bermuda-de-jean.png' },
+    { id: 1, nombre: 'Remera diseño de Messi', descripcion: 'Remera Manga Corta de Algodon', precio: 20, imagen: '../img/remera1.jpeg', tipo: 1},
+    { id: 2, nombre: 'Remera diseño de Turing', descripcion: 'Remera Manga Corta de Algodon', precio: 80, imagen: '../img/remera2.jpeg', tipo: 1},
+    { id: 3, nombre: 'Remera diseño de Boxeo', descripcion: 'Remera Manga Corta de Algodon', precio: 900, imagen: '../img/remera3.jpeg', tipo: 1 },
     //{ id: 2, nombre: 'Bermuda', descripcion: 'Bermuda de Jean', precio: 80, imagen: '../img/bermuda-de-jean.png' },
     //{ id: 2, nombre: 'Bermuda', descripcion: 'Bermuda de Jean', precio: 80, imagen: '../img/bermuda-de-jean.png' },
     //{ id: 2, nombre: 'Bermuda', descripcion: 'Bermuda de Jean', precio: 80, imagen: '../img/bermuda-de-jean.png' },
@@ -12,7 +12,7 @@ const productos = [
 
 //Genero el catalogo
 function cargarCatalogo() {
-    const catalogo = document.getElementById('catalogo');
+    const catalogo = document.getElementById('catalogo_remeras');
 
     productos.forEach(producto => {
         //Creo la estructura
@@ -20,12 +20,13 @@ function cargarCatalogo() {
         divProducto.classList.add('producto');
 
         divProducto.innerHTML = `
-            <img src="${producto.imagen}" alt="${producto.nombre}">
+        <img src="${producto.imagen}" alt="${producto.nombre}">
         <div class="contenido">
             <h3>${producto.nombre}</h3>
-            <p>${producto.descripcion}</p>
-            <p class="precio">$${producto.precio}</p>
-            <button>Comprar</button>
+            <div class="precio_boton">
+                <button>Comprar</button>
+                <p class="precio">$${producto.precio}</p>
+            </div>
         </div>
         `
         catalogo.appendChild(divProducto);
