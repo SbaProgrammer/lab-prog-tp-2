5049ef889b3904b187e957613cd48193413d6da9
Bautista Fernandez <bautista.fernandez@est.fi.uncoma.edu.ar>
2024-10-17 17:16:00 -0300

    elimino código viejo

--- js/contacto.js
+++ js/contacto.js
@@ -1,40 +1,3 @@
-const entradas = document.querySelectorAll(".entrada");
-/* 
-Selecciona todos los elementos que tienen la clase entradaForm, que serian los subformularios 
-*/
-
-function focusFunc() {
-  let parent = this.parentNode;
-  parent.classList.add("focus");
-}
-
-/* 
-Esta funcion se ejecuta solo en los ejemplos donde el usuario hace foco, es decir
-que se pare en el elemento o asi, si hacen foco le agrega la clase focus
-a ese elemento.
-*/
-
-function blurFunc() {
-  let parent = this.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
-  }
-}
-
-/* 
-Esta funcion se ejecuta en cada nodo para ver si el valor de adentro suyo es vacio o no
-si no es vacio entonces no hace nada y si es vacio entonces remueve la clase focus
-*/
-
-entradas.forEach((entrada) => {
-    entrada.addEventListener("focus", focusFunc);
-    entrada.addEventListener("blur", blurFunc);
-});
-
-/* 
-A cada elemento se le añade un event listener que serian las funciones.
-*/
-
 document.getElementById("contactForm").addEventListener("submit", function(event) {
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
@@ -44,13 +7,13 @@ document.getElementById("contactForm").addEventListener("submit", function(event
       alert("Por favor, complete todos los campos.");
       event.preventDefault();
   } else {
-      alert("¡Formulario enviado con éxito!");
+      alert("Formulario enviado con éxito");
   }
 });
 
 
 document.getElementById("contactForm").addEventListener("submit", function(event) {
-  event.preventDefault();  // Evita que el formulario se envíe a una página de error
+  event.preventDefault(); 
   alert("¡Gracias por contactarnos! Te responderemos pronto.");
 });
 
