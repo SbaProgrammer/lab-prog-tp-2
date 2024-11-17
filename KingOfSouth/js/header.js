beadef408b9ca1910320f9be3549d8c58d8a6b93
Nicanor Fernandez <nicanor.fernandez@est.fi.uncoma.edu.ar>
2024-11-17 17:36:13 -0300

    Cambios minimos en el index

--- KingOfSouth/js/header.js
+++ KingOfSouth/js/header.js
@@ -1,11 +1,11 @@
 const header = document.querySelector('header');
 
 header.innerHTML = `
-    <a href="#" class="logo">King of The South</a>
+    <a href="/" class="logo">King of The South</a>
         <ul class="navlist">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="catalogo.html">Catalogo</a></li>
-            <li><a href="contacto.html">Sobre Nosotros</a></li>
+        <li><a href="catalogo.html">Catalogo</a></li>
+        <li><a href="contacto.html">Sobre Nosotros</a></li>
+        <li><a href="index.html">Carrito</a></li>
         </ul>
     <div class="bx bx-menu" id="menu-icon"> </div>
 `
